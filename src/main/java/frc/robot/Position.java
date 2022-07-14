e8b189079c57074447d9d4cd6df1b0ae6b0d06be
Markee <73059112+ItsMeMarkee@users.noreply.github.com>
2022-07-14 22:27:24 +0000

    Gremlin Attack

--- src/main/java/frc/robot/Position.java
+++ src/main/java/frc/robot/Position.java
@@ -5,28 +5,95 @@ import static lib.Romi.turnDegrees;
 
 public class Position {
 
+  int xPosition; 
+  int yPosition;
+  char faceDirection;
+
 
     public void driveUp() throws InterruptedException {
 
-    }
+      if(faceDirection == 'e') {
+        turnDegrees(-90);
+      }
+      else if (faceDirection == 's') {
+        turnDegrees(180);
+      }
+      else if (faceDirection == 'w') {
+        turnDegrees(90);
+      }
+      faceDirection = 'n';
+
+      driveDistance(10);
+
+      yPosition = yPosition + 10;
+      }
+    
+
 
     public void driveDown() throws InterruptedException {
+      
+      if(faceDirection == 'e') {
+        turnDegrees(90);
+      }
+      else if (faceDirection == 'n') {
+        turnDegrees(-180);
+      }
+      else if (faceDirection == 'w') {
+        turnDegrees(-90);
+      }
+      faceDirection = 's';
 
-    }
+      driveDistance(10);
+
+      yPosition = yPosition - 10;
+      }
+    
 
     public void driveRight() throws InterruptedException {
 
-    }
+      if(faceDirection == 'n') {
+        turnDegrees(90);
+      }
+      else if (faceDirection == 's') {
+        turnDegrees(-90);
+      }
+      else if (faceDirection == 'w') {
+        turnDegrees(180);
+      }
+      faceDirection = 'e';
+
+      driveDistance(10);
+
+      xPosition = xPosition + 10;
+      }        
+
+    
 
     public void driveLeft() throws InterruptedException {
+      
+      if(faceDirection == 'n') {
+        turnDegrees(-90);
+      }
+      else if (faceDirection == 's') {
+        turnDegrees(90);
+      }
+      else if (faceDirection == 'e') {
+        turnDegrees(180);
+      }
+      faceDirection = 'w';
 
-    }
+      driveDistance(10);
+
+      xPosition = xPosition - 10;
+      }        
+    
 
     public void returnToHome() {
 
     }
 
     public String toString() {
-        return "";
+
+      return "(" + xPosition + ", " + yPosition + ")";
     }
 }
